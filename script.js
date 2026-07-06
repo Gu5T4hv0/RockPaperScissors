e9592952e97f3f026333d622d17a86eff8439b27
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-06 17:39:26 -0300

    playround

--- script.js
+++ script.js
@@ -9,15 +9,39 @@ function getHumanChoice() {
     }
 }
 const humanChoice = getHumanChoice();
+console.log(humanChoice);
 
 function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const computerChoice = getComputerChoice(1, 3);
+console.log(computerChoice);
 
 function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice, computerChoice)
+    if (humanChoice === 1 && computerChoice === 1) {
+        return 'Tie! - Human:Rock | Computer:Rock';
+    } else if (humanChoice === 2 && computerChoice === 2) {
+        return 'Tie! - Human:Paper | Computer:Paper';
+    } else if (humanChoice === 3 && computerChoice === 3) {
+        return 'Tie! - Human:Scissors | Computer:Scissors';
+    }
+    else if (humanChoice === 1 && computerChoice === 2) {
+        return 'You Lost! - Human:Rock | Computer:Paper';
+    } else if (humanChoice === 1 && computerChoice === 3) {
+        return 'You Won! - Human:Rock | Computer:Scissors';
+    } 
+    else if (humanChoice === 2 && computerChoice === 1) {
+        return 'You Won! - Human:Paper | Computer:Rock';
+    } else if (humanChoice === 2 && computerChoice === 3) {
+        return 'You Lost! - Human:Paper | Computer:Scissors';
+    }
+    else if (humanChoice === 3 && computerChoice === 1) {
+        return 'You Lost! - Human:Scissors | Computer:Rock';
+    } else if (humanChoice === 3 && computerChoice === 2) {
+        return 'You Won! - Human:Scissors | Computer:Paper';
+    }
 }
+console.log(playRound());
 
 const humanScore = 0;
 const computerScore = 0;
