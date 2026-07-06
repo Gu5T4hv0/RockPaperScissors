bb4edce5275569f0dc7e80ae27c6c6af1709a976
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-06 17:01:53 -0300

    mudei uma pa de coisa, vamo ver agr

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandom(1, 3));
-
-// function getComputerChoice(random) {
-//     return random;
-// }
-
 function getHumanChoice() {
-    const humanChoice = prompt("Rock Paper Scissors: ");
-    if (humanChoice.toLowerCase() === 'rock') {
+    const choice = prompt("Rock Paper Scissors: ");
+    if (choice.toLowerCase() === 'rock') {
         return 1;
-    } else if (humanChoice.toLowerCase() === 'paper') {
+    } else if (choice.toLowerCase() === 'paper') {
         return 2;
-    } else if (humanChoice.toLowerCase() === 'scissors') {
+    } else if (choice.toLowerCase() === 'scissors') {
         return 3;
     }
 }
+const humanChoice = getHumanChoice();
 
-function game(getComputerChoice) {
-
+function getComputerChoice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+const computerChoice = getComputerChoice(1, 3);
 
-// console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice, computerChoice)
+}
 
 const humanScore = 0;
 const computerScore = 0;
