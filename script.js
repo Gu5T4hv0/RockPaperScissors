11c3497992aa9de821bcf9e4b745a16b2cfec0ad
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-06 16:18:30 -0300

    humano escrito

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    
+    choice = prompt("Rock Paper Scissors: ");
+    if (choice.toLowerCase() === "rock") {
+        return 1;
+    } else if (choice.toLowerCase() === 'paper') {
+        return 2;
+    } else if (choice.toLowerCase() === 'scissors') {
+        return 3;
+    }
 }
+
+console.log(getHumanChoice());
