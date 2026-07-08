2e62bcbb5ab29651f2aa264e94d1999a653def0f
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-08 18:10:49 -0300

    its finished

--- script.js
+++ script.js
@@ -14,55 +14,50 @@ function getComputerChoice(min, max) {
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(1, 3);
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log('Tie! - Human:Rock | Computer:Rock');
+    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        console.log('Tie! - Human:Paper | Computer:Paper');
+    } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+        console.log('Tie! - Human:Scissors | Computer:Scissors');
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore += 1;
+        console.log('You Lost! - Human:Rock | Computer:Paper');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        humanScore += 1;
+        console.log('You Won! - Human:Rock | Computer:Scissors');
+    } 
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        humanScore += 1;
+        console.log('You Won! - Human:Paper | Computer:Rock');
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        computerScore += 1;
+        console.log('You Lost! - Human:Paper | Computer:Scissors');
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore += 1;
+        console.log('You Lost! - Human:Scissors | Computer:Rock');
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore += 1;
+        console.log('You Won! - Human:Scissors | Computer:Paper');
+    }
+}
 
-function playGame(humanChoose, computerChoose) {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
+function playGame() {
     for (i = 0; i < 5; i++) {
-        getHumanChoice();
-        getComputerChoice(1, 3);
-        function playRound(humanChoice, computerChoice) {
-
-            if (humanChoice === 'rock' && computerChoice === 'rock') {
-                console.log('Tie! - Human:Rock | Computer:Rock');
-            } else if (humanChoice === 'paper' && computerChoice === 'paper') {
-                console.log('Tie! - Human:Paper | Computer:Paper');
-            } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
-                console.log('Tie! - Human:Scissors | Computer:Scissors');
-            }
-            else if (humanChoice === 'rock' && computerChoice === 'paper') {
-                computerScore += 1;
-                console.log('You Lost! - Human:Rock | Computer:Paper');
-            } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-                humanScore += 1;
-                console.log('You Won! - Human:Rock | Computer:Scissors');
-            } 
-            else if (humanChoice === 'paper' && computerChoice === 'rock') {
-                humanScore += 1;
-                console.log('You Won! - Human:Paper | Computer:Rock');
-            } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-                computerScore += 1;
-                console.log('You Lost! - Human:Paper | Computer:Scissors');
-            }
-            else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-                computerScore += 1;
-                console.log('You Lost! - Human:Scissors | Computer:Rock');
-            } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-                humanScore += 1;
-                console.log('You Won! - Human:Scissors | Computer:Paper');
-            }
-        }
-        playRound(humanChoose, computerChoose);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice(1, 3);
+        
+        playRound(humanSelection, computerSelection);
     }
     
     return `Human: ${humanScore} Computer: ${computerScore}`;
-    
-    
 }
-const result = playGame(humanSelection, computerSelection);
-console.log(result);
 
-// console.log(playGame(humanSelection, computerSelection));
+const result = playGame();
+console.log(result);
