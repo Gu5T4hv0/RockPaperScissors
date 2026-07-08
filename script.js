e9845190980c24b523b86f953d559ddbd8ef96ec
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-08 13:37:25 -0300

    só pra salvar

--- script.js
+++ script.js
@@ -9,17 +9,15 @@ function getHumanChoice() {
     }
 }
 const humanChoice = getHumanChoice();
-// console.log(humanChoice);
 
 function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const computerChoice = getComputerChoice(1, 3);
-// console.log(computerChoice);
 
 let humanScore = 0;
 let computerScore = 0;
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === 1 && computerChoice === 1) {
         return 'Tie! - Human:Rock | Computer:Rock';
@@ -30,30 +28,23 @@ function playRound(humanChoice, computerChoice, humanScore, computerScore) {
     }
     else if (humanChoice === 1 && computerChoice === 2) {
         return 'You Lost! - Human:Rock | Computer:Paper';
-        computerScore += 1;
-        console.log(`Computer: ${computerScore}`);
     } else if (humanChoice === 1 && computerChoice === 3) {
         return 'You Won! - Human:Rock | Computer:Scissors';
-        humanScore += 1;
-        console.log(`Human: ${humanScore}`);
     } 
     else if (humanChoice === 2 && computerChoice === 1) {
         return 'You Won! - Human:Paper | Computer:Rock';
-        humanScore += 1;
-        console.log(`Human: ${humanScore}`);
     } else if (humanChoice === 2 && computerChoice === 3) {
         return 'You Lost! - Human:Paper | Computer:Scissors';
-        computerScore += 1;
-        console.log(`Computer: ${computerScore}`);
     }
     else if (humanChoice === 3 && computerChoice === 1) {
         return 'You Lost! - Human:Scissors | Computer:Rock';
-        computerScore += 1;
-        console.log(`Computer: ${computerScore}`);
     } else if (humanChoice === 3 && computerChoice === 2) {
         return 'You Won! - Human:Scissors | Computer:Paper';
-        humanScore += 1;
-        console.log(`Human: ${humanScore}`);
     }
 }
-console.log(playRound(humanChoice, computerChoice));
+// const playRoundResult = playRound(humanChoice, computerChoice);
+// console.log(playRoundResult);
+
+// for (i = 0; i < 5; i++) {
+//     playRound(humanChoice, computerChoice);
+// }
